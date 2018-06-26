13dcf8114ccce118c970e7698aef22e115ddd44f
Mustansir Zia <mustansir29@hotmail.com>
2018-06-26 22:23:51 +0530

    polyfills.js added

---
+++ src/polyfills.js
@@ -0,0 +1,13 @@
+
+// Polyfill to make input type="number" work for all non HTML5 browsers and Safari.
+(function() {
+    if ($) {
+        $(function () {
+            $('input[type="number"]').keypress(function (e) {
+                if (isNaN(e.key)) {
+                    e.preventDefault();
+                } 
+            });
+        });
+    }
+})()
